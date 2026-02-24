3aca68ee4d09d20842eec4769e98e7b0fc55ca7d
Gregory D. <157378532+gp06drap@users.noreply.github.com>
2026-02-24 14:26:29 +0000

    2/24

---
+++ other/myForm.js
@@ -0,0 +1,11 @@
+/*console.log("Hello World");
+alert("Hello World");
+
+const buttonA=document.querySelector("#button_A");
+const headingA=document.getElementById("heading_A");
+
+buttonA.addEventListener('click', ()=>{
+    const name=prompt("What is your name?");
+    alert('Hello '+name+', nice to see you!');
+    headingA.textContent=`Welcome ${name}`;
+});*/
