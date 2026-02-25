4394a02cec50756a6742dc378aaf9f51f2065676
Gregory D. <157378532+gp06drap@users.noreply.github.com>
2026-02-25 14:06:59 +0000

    Up to JS Pre-lab practise

--- other/lab4a/script.js
+++ other/lab4a/script.js
@@ -1,1 +1,48 @@
+const submitB=document.querySelector("button");
+const userInput=document.querySelector("#guess");
+const response=document.querySelector("#answer")
+let guesses;
+const goal=parseInt(Math.random()*100)+1;
 
+submitB.addEventListener("click", function(){
+    let value=userInput.value;
+    let side;
+    if (guesses===null){
+        guesses=value;
+    }else{
+        guesses=guesses+" "+value;
+    }
+    if(value!=goal){
+        if(value<goal){
+            side="low";
+        }else{
+            side="high";
+        }
+        response.innerHTML=`<p>Previous guesses: ${guesses}</p><p style="background-color:red;color:white;">Wrong</p><p>Last guess was too ${side}!</p`;
+    }else{
+        response.innerHTML=`<p>Previous guesses: ${guesses}</p><p style="background-color:green;color:white;">Congratulations! You got it right!</p>`;
+    }
+});
+
+const buttonA=document.querySelector("#button_A");
+const headingA=document.getElementById("heading_A");
+
+buttonA.addEventListener('click', ()=>{
+    const name=prompt("What is your name?");
+    alert('Hello '+name+', nice to see you!');
+    headingA.textContent=`Welcome ${name}`;
+});
+
+const textBox=document.querySelector("#textBox");
+const out=document.querySelector("#output");
+textBox.addEventListener("keydown", (event)=>{
+    out.textContent=`You pressed "${event.key}".`
+});
+
+const buttonB=document.querySelector("#button_B");
+const greeting=document.getElementById("greeting");
+
+buttonB.addEventListener('click', ()=>{
+    const name=prompt("What is your name?");
+    greeting.textContent=`Hello! ${name}, nice to see you!`;
+});
--- other/myForm.js
+++ other/myForm.js
@@ -1,5 +1,5 @@
 /*console.log("Hello World");
-alert("Hello World");
+alert("Hello World");*/
 
 const buttonA=document.querySelector("#button_A");
 const headingA=document.getElementById("heading_A");
@@ -8,4 +8,4 @@ buttonA.addEventListener('click', ()=>{
     const name=prompt("What is your name?");
     alert('Hello '+name+', nice to see you!');
     headingA.textContent=`Welcome ${name}`;
-});*/
+});
